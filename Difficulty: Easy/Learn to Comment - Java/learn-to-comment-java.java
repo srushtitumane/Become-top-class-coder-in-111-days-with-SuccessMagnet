af66a043a181e6bdafae4f86f2895f29f557338b
Srushti Tumane <shrushtitumane1@gmail.com>
2025-07-29 20:58:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Learn to Comment - Java/learn-to-comment-java.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); // for taking input
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        // Prints a
+        System.out.println(a);
+        
+        // Don't print b, as per the expected output
+
+        // Prints c
+        System.out.println(c);
+    }
+}

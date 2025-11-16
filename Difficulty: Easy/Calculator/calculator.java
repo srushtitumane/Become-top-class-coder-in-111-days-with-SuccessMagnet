9088379ad1a270652f56f80a9d3d50988ed8494b
Srushti Tumane <shrushtitumane1@gmail.com>
2025-11-16 14:30:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Calculator/calculator.java
+++ Difficulty: Easy/Calculator/calculator.java
@@ -1,25 +1,22 @@
 class Solution {
-    public static void utility(int a, int b, int opr) {
-        // code here
+    public void calculate(int a, int b, int operator) {
+
         int result;
-        if(opr==1)
-        {
-            result=a+b;
-            System.out.print(result);
+
+        if (operator == 1) {
+            result = a + b;
+            System.out.println(result);
         }
-        else if(opr==2)
-        {
-            result=a-b;
-            System.out.print(result);
+        else if (operator == 2) {
+            result = a - b;
+            System.out.println(result);
         }
-        else if(opr==3)
-        {
-            result=a*b;
-            System.out.print(result);
+        else if (operator == 3) {
+            result = a * b;
+            System.out.println(result);
         }
-        else
-        {
-            System.out.print("Invalid Input");
+        else {
+            System.out.println("Invalid Input");
         }
     }
 }

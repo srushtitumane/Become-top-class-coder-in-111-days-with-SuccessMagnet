548428564cbb4c0132519d1959802b72b810002a
Srushti Tumane <shrushtitumane1@gmail.com>
2025-12-13 20:32:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Swap diagonals/swap-diagonals.java
@@ -0,0 +1,11 @@
+class Solution {
+    public void swapDiagonal(int[][] mat) {
+        int n = mat.length;
+
+        for (int i = 0; i < n; i++) {
+            int temp = mat[i][i];
+            mat[i][i] = mat[i][n - 1 - i];
+            mat[i][n - 1 - i] = temp;
+        }
+    }
+}

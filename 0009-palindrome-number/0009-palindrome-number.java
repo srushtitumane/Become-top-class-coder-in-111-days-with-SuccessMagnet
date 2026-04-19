72213b919c726547769d43f0b45c6412b871e35f
Srushti Tumane <shrushtitumane1@gmail.com>
2026-04-19 23:28:24 +0530

    Time: 5 ms (79.57%), Space: 45.8 MB (75.9%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        
+        if (x < 0) return false;
+        int temp = x;
+        int rev = 0;
+        while (temp != 0) {
+            int rem = temp % 10;
+            rev = rev * 10 + rem;
+            temp = temp / 10;
+        }
+        return rev == x;
+    }
+}

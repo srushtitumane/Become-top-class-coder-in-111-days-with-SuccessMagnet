d5ab6164cccfa8fdb76609512a72c81b6adebe6d
Srushti Tumane <shrushtitumane1@gmail.com>
2026-06-17 21:15:19 +0530

    Time: 6 ms (87.34%), Space: 43.9 MB (74.84%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,32 +1,45 @@
 class Solution {
+
     public boolean checkInclusion(String s1, String s2) {
-        int[] freq=new int[26];
-        for(char c:s1.toCharArray()){
-            freq[c-'a']++;
+
+        int alpha[] = new int[26];
+
+        for (char c : s1.toCharArray()) {
+            alpha[c - 'a']++;
         }
-        int i=0,j=0;
-        while(j<s2.length()){
-            freq[s2.charAt(j)-'a']--;
-            while((j-i+1)>s1.length()){
-                freq[s2.charAt(i)-'a']++;
-                i++;
+
+        int low = 0, high = 0;
+
+        while (high < s2.length()) {
+
+            char jc = s2.charAt(high);
+            alpha[jc - 'a']--;
+
+            while ((high - low + 1) > s1.length()) {
+
+                char ic = s2.charAt(low);
+                alpha[ic - 'a']++;
+                low++;
             }
-            if(isZeros(freq)) {
-            return true;
+
+            if (isZeros(alpha)) {
+                return true;
             }
-            j++;
+
+            high++;
         }
-        
-        return false;    
-        
+
+        return false;
     }
-    public boolean isZeros(int[] freq){
-        for(int x:freq){
-            if(x!=0){
+
+    boolean isZeros(int[] alpha) {
+
+        for (int x : alpha) {
+            if (x != 0) {
                 return false;
             }
         }
+
         return true;
     }
-
 }

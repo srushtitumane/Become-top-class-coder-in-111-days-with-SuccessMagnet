4c28ed76c7a98fe099d501668acff105d7d1e1d9
Srushti Tumane <shrushtitumane1@gmail.com>
2026-06-17 20:54:48 +0530

    Time: 0 ms (100%), Space: 46.7 MB (51.64%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -14,12 +14,11 @@ public class Solution {
         ListNode slow=head;
         ListNode fast=head;
         while(fast!=null && fast.next!=null){
-            slow =slow.next;
+            slow=slow.next;
             fast=fast.next.next;
             if(slow==fast){
                 return true;
             }
-            
         }
         return false;
         

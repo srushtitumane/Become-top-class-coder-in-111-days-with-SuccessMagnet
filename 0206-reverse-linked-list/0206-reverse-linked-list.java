824fb1cf14244c0323ee61ee7d7691de7610af1e
Srushti Tumane <shrushtitumane1@gmail.com>
2026-06-29 20:38:00 +0530

    Time: 0 ms (100%), Space: 44.4 MB (30.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,16 +10,21 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr=head;
-        while(curr!=null){
-            ListNode nxt=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nxt;
+
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            ListNode next = curr.next;
+
+            curr.next = prev;
+
+            prev = curr;
+
+            curr = next;
         }
+
         return prev;
-        
     }
-    
 }
